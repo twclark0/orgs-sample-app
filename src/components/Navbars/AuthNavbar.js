7509c795e9a48348d7f0e4abe7360d4ac4cfa843
twclark0 <tylerc644@gmail.com>
2021-03-23 14:14:16 -0600

    Add redirect after authetication to admin page

--- src/components/Navbars/AuthNavbar.js
+++ src/components/Navbars/AuthNavbar.js
@@ -13,8 +13,13 @@ export default function Navbar() {
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link
               className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
+              style={{ display: "flex", alignItems: "center" }}
               to="/"
             >
+              <img
+                style={{ height: "40px" }}
+                src="https://cdn.auth0.com/blog/chat0/logo.jpeg"
+              />
               Chat0
             </Link>
             <button
--- src/index.js
+++ src/index.js
@@ -5,8 +5,32 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 import Admin from "layouts/Admin.js";
 import Landing from "views/Landing.js";
-import { Auth0Provider } from "@auth0/auth0-react";
-import AuthWrapper from "components/AuthWrapper/AuthWrapper";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+
+const App = () => {
+  const { isLoading, error, isAuthenticated } = useAuth0();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {isAuthenticated ? (
+            <Redirect to="/admin" component={Admin} />
+          ) : (
+            <Route path="/" component={Landing} />
+          )}
+        </Route>
+        <Route path="/admin" component={Admin} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <Auth0Provider
@@ -14,15 +38,7 @@ ReactDOM.render(
     clientId="OgdvG5snvJGpeIrW9FKXbB5D9UmSQufP"
     redirectUri={window.location.origin}
   >
-    <AuthWrapper>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/admin" component={Admin} />
-          <Route path="/" exact component={Landing} />
-          <Redirect from="*" to="/" />
-        </Switch>
-      </BrowserRouter>
-    </AuthWrapper>
+    <App />
   </Auth0Provider>,
   document.getElementById("root")
 );
