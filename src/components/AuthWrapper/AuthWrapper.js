11bbf94d349ba3681c29cd437696674d53227e29
twclark0 <tylerc644@gmail.com>
2021-03-30 08:49:35 -0600

    Add conditional logic to React SDK wrapper for orgs

--- src/components/AuthWrapper/AuthWrapper.js
+++ src/components/AuthWrapper/AuthWrapper.js
@@ -1,1 +1,13 @@
+import React from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
 
+export const AuthWrapper = ({ children }) => {
+  const curOrganization = localStorage.getItem("organization_id");
+  const options = {
+    domain: "dev-rm3u1yq4.auth0.com",
+    clientId: "OgdvG5snvJGpeIrW9FKXbB5D9UmSQufP",
+    redirectUri: window.location.origin,
+    ...(curOrganization ? { organization: curOrganization } : null)
+  };
+  return <Auth0Provider {...options}>{children}</Auth0Provider>;
+};
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 import Admin from "layouts/Admin.js";
 import Landing from "views/Landing.js";
-import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { AuthWrapper } from "components/AuthWrapper/AuthWrapper";
 
 const App = () => {
   const { isLoading, error, isAuthenticated } = useAuth0();
@@ -33,13 +34,8 @@ const App = () => {
 };
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="dev-rm3u1yq4.auth0.com"
-    clientId="OgdvG5snvJGpeIrW9FKXbB5D9UmSQufP"
-    redirectUri={window.location.origin}
-    organization={localStorage.getItem("organization_id")}
-  >
+  <AuthWrapper>
     <App />
-  </Auth0Provider>,
+  </AuthWrapper>,
   document.getElementById("root")
 );
