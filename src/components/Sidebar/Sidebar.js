5b931e9271e7dd624e26b9b7466371f580571be9
twclark0 <tylerc644@gmail.com>
2021-03-31 10:29:51 -0600

    Conditionally check for org_id

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -12,10 +12,10 @@ export default function Sidebar() {
   const { user } = useAuth0();
   const currentOrg = organizationMetaData.find((o) => o.id === user?.org_id);
   useEffect(() => {
-    if (user.org_id) {
+    if (user?.org_id) {
       localStorage.setItem("organization_id", String(user.org_id));
     }
-  }, [user.org_id]);
+  }, [user?.org_id]);
   return (
     <>
       <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-no-wrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
